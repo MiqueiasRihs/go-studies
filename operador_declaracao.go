1700b55fc896ca346da1966a96e259fab8ee2ab0
Miqueias <miqueias.rihs@gmail.com>
2022-09-20 19:31:10 -0300

    cap2 e 3

--- operador_declaracao.go
+++ operador_declaracao.go
@@ -4,12 +4,21 @@ import (
 	"fmt"
 )
 
+var y = "olá bom dia"
+
 func main() {
 
 	x := 10
-	y := "Ola bom dia"
 
-	fmt.Println("x: %v, %T\n, x, x")
-	fmt.Println("y: %v, %T, y, y")
+	fmt.Printf("x: %v, %T\n", x, x)
+	fmt.Printf("y: %v, %T\n\n", y, y)
+	// := Operador de declaracao, com ele é defenida pelo menos uma nova variavel
+	// e ela é inserida apenas no code blocks
+	// Ex: x := 20 nao irá funcionar pois a variavel x ja foi delacarada a cima
+	// Então com ela podemos apenas fazer uma atribuição. Ex: x = 20
+
+	x, z := 20, 30
+	fmt.Printf("x: %v, %T\n", x, x)
+	fmt.Printf("z: %v, %T", z, z)
 
 }
